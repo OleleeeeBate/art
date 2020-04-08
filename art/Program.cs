9f3ada6e6b63fe2b3d74e0f7b0c81707454f81dd
margaritovamariya <margaritovamariya@gmail.com>
2020-04-08 15:56:30 +0300

    Add project files.

---
+++ art/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace art
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
